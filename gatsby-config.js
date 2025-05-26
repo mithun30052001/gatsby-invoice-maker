643a88fb2b5eb284fe3c75a065572577cd1e73fb
mithun30052001 <lmithun01@gmail.com>
2025-05-26 22:01:57 +0530

    gatsby config and gatsby related packages installed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,26 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
+require("dotenv").config();
+const https = require("https");
+https.globalAgent.options.rejectUnauthorized = false
 
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: "Invoice Maker",
   },
   plugins: [
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-source-strapi",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        apiURL: process.env.STRAPI_API_URL || "https://invoice-strapi.onrender.com/",
+        collectionTypes: ["invoice", "client"],
+        accessToken: process.env.STRAPI_API_TOKEN,
+        singleTypes: [],
+        downloadFile: true,
+				httpsAgent: new https.Agent({
+					rejectUnauthorized: false,
+				})
       },
     },
   ],
-}
+};
